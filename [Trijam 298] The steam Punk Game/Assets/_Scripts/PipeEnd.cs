0beb3a68ee8679a27b99bd3a05ffbfa5fab571a0
Stealthness <stevewestinlondon@gmail.com>
2024-11-29 22:16:30 +0000

    working on steam effect

--- [Trijam 298] The steam Punk Game/Assets/_Scripts/PipeEnd.cs
+++ [Trijam 298] The steam Punk Game/Assets/_Scripts/PipeEnd.cs
@@ -1,16 +1,72 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class PipeEnd : MonoBehaviour
+namespace _Scripts
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class PipeEnd : MonoBehaviour
     {
         
-    }
+        public Sprite fixedPipedEnd;
+        public Sprite brokenPipeEnd;
+        private SpriteRenderer _pipeSpriteRenderer;
+        private SpriteRenderer _steamSpriteRenderer;
+        public GameObject steam;
+        public GameObject fixSymbolObject;
+        private Animator _steamAnimator;
+        
+        
 
-    // Update is called once per frame
-    void Update()
-    {
+        private void Awake()
+        {
+            _pipeSpriteRenderer= GetComponent<SpriteRenderer>();
+        }
+
+
+        private void Start()
+        {
+            _steamSpriteRenderer = steam.GetComponent<SpriteRenderer>();
+            _steamAnimator =steam.GetComponent<Animator>();
+            Invoke(nameof(DestroyPipe), Random.Range(5f, 10f));
+            FixPipe();
+        }
+    
+        public void DestroyPipe()
+        {
+            ToggleSteam(true);
+            _steamAnimator.Play("Steam");
+        }
+
+        public void FixPipe()
+        {
+            ToggleSteam(false);
+        }
         
+        private void ToggleSteam(bool state)
+        {
+            _pipeSpriteRenderer.sprite = state ? brokenPipeEnd : fixedPipedEnd;
+            _steamSpriteRenderer.enabled = state;
+            _steamAnimator.enabled = state;
+        }
+        
+        
+        
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                Debug.Log("Player has reached the end of the pipe");
+                fixSymbolObject.GetComponent<SpriteRenderer>().enabled = true;
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                fixSymbolObject.GetComponent<SpriteRenderer>().enabled = false;
+            }
+        }
     }
 }
