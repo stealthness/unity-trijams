e74493cde0f438a89c4eddeefb65f05099229616
Stealthness <stevewestinlondon@gmail.com>
2024-12-01 20:36:46 +0000

    add simple controls

--- [297] Steam/Assets/_Scripts/Player/PlayerController.cs
+++ [297] Steam/Assets/_Scripts/Player/PlayerController.cs
@@ -1,12 +1,34 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace _Scripts.Player
 {
     public class PlayerController : MonoBehaviour
     {
+        
+        Vector2 _dir;
+        
         private void Start()
         {
             Debug.Log("Player Controller is ready");
+        }
+        
+        private void Update()
+        {
+            transform.position += new Vector3(_dir.x, _dir.y, 0) * Time.deltaTime;
+        }
+        
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                _dir = context.ReadValue<Vector2>();
+            }
+
+            if (context.canceled)
+            {
+                _dir = Vector2.zero;
+            }
         }
     }
 }
