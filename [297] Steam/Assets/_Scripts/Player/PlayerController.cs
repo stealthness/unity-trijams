e20f31f78686f9d0c05e142f22ad89f2819f280e
Stealthness <stevewestinlondon@gmail.com>
2024-12-01 20:14:02 +0000

    copying template Scripts

---
+++ [297] Steam/Assets/_Scripts/Player/PlayerController.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace _Scripts.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Player Controller is ready");
+        }
+    }
+}
