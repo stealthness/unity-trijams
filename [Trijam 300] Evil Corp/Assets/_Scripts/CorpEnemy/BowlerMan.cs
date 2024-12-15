369bceb359b137b279044567c2fcc40cfbbe061f
Stealthness <stevewestinlondon@gmail.com>
2024-12-15 12:32:14 +0000

    added BowlerMan patrol

--- [Trijam 300] Evil Corp/Assets/_Scripts/CorpEnemy/BowlerMan.cs
+++ [Trijam 300] Evil Corp/Assets/_Scripts/CorpEnemy/BowlerMan.cs
@@ -1,15 +1,29 @@
+using System;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 namespace _Scripts.CorpEnemy
 {
     public class BowlerMan : MonoBehaviour, IDamageable
     {
         public int health = 100;
-        
-        public void TakeDamage()
+        public Transform[] patrolPoints;
+        private int currentPatrolIndex = 0;
+
+        private void Update()
         {
-            throw new System.NotImplementedException();
+            MovetToNextPatrolPoint();
         }
+
+        private void MovetToNextPatrolPoint()
+        {
+            if (Vector2.Distance(transform.position, patrolPoints[currentPatrolIndex].position) < 0.1f)
+            {
+                currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Length;
+            }
+            GetComponent<Rigidbody2D>().linearVelocityX =  patrolPoints[currentPatrolIndex].position.x - transform.position.x < 0  ? 1 : -1;
+        }
+
 
         public void TakeDamage(int damage)
         {
