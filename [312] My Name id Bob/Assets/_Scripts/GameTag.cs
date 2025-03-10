7fb1cf72bc6f321df6d592b626d407a9b12bfe8c
Stealthness <stevewestinlondon@gmail.com>
2025-03-10 17:22:26 +0000

    GameTags added

---
+++ [312] My Name id Bob/Assets/_Scripts/GameTag.cs
@@ -0,0 +1,12 @@
+namespace _Scripts
+{
+    public static class GameTag
+    {
+        public const string Player = "Player";
+        public const string Boundary = "Boundary";
+        public const string Lava = "Lava";
+        public const string Platform = "Platform";
+    }
+
+
+}
